49a865289d768d1312e9ac67c867df82ac709bd6
81520 <815204388@qq.com>
2024-10-23 19:45:22 +0800

    修正样式不显示问题

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/FAA-WebSite/",
 
   lang: "zh-CN",
   title: "文档演示",
